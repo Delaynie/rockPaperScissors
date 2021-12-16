6259f1fd04b62d12361ada8ec3eed0ba28d22d4c
Delaynie Wheeler <delaynie93@gmail.com>
2021-12-16 13:33:49 -0600

    code playRound function

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 console.log("It's working!");
 
-/* function for computer play mode to log a random response */
+/* function for computer play mode to log a random response*/
 function computerPlay() {
     arr = new Array(3);
         arr[0] = "rock";
@@ -9,7 +9,46 @@ function computerPlay() {
         arr[2] = "scissors";
     let random = Math.floor(Math.random()*arr.length);
     let randomWord = random;
-    console.log(arr[randomWord]);
+    //console.log(arr[randomWord]);
+    return arr[randomWord];
 }
+//computerPlay();
 
-computerPlay();
+// game play
+
+function playRound(playerSelection, computerSelection) {
+    //console.log("it's working");
+    console.log(playerSelection, computerSelection);
+    // if paper 
+    if (playerSelection == "paper" && computerSelection == "rock") {
+        console.log("Paper covers rock, you win!");
+    } else if (playerSelection == "paper" && computerSelection == "scissors") {
+        console.log("Scissors cuts papers, you lose!");
+    } // if it's a tie
+    else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    }
+    //if rock
+    else if (playerSelection == "rock" && computerSelection == "scissors") {
+        console.log("Rock breaks scissors, you win!");
+    } else if (playerSelection == "rock" && computerSelection == "paper") {
+        console.log("Paper covers rock, you lose!");
+    } // if it's a tie
+    else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    }
+    //if scissors
+    else if (playerSelection == "scissors" && computerSelection == "rock") {
+        console.log("Rock breaks scissors, you lose!");
+    } else if (playerSelection == "scissors" && computerSelection == "paper") {
+        console.log("Scissors cut paper, you win!");
+    } // if it's a tie
+    else if (playerSelection == computerSelection) {
+        console.log("It's a tie!");
+    }
+}   
+
+let playerSelection = "rock";
+const computerSelection = computerPlay();
+
+game(playerSelection, computerSelection);
