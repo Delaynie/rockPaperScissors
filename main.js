176a750059a2d26f2c06a108ae542d49e81528d7
Delaynie Wheeler <delaynie93@gmail.com>
2021-12-15 18:53:06 -0600

    create computerPlay function

--- main.js
+++ main.js
@@ -1,2 +1,15 @@
 
 console.log("It's working!");
+
+/* function for computer play mode to log a random response */
+function computerPlay() {
+    arr = new Array(3);
+        arr[0] = "rock";
+        arr[1] = "paper";
+        arr[2] = "scissors";
+    let random = Math.floor(Math.random()*arr.length);
+    let randomWord = random;
+    console.log(arr[randomWord]);
+}
+
+computerPlay();
