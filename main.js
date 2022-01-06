f46f5efb9ef583aa0474e298b5e58d36ccea8c3c
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-06 13:06:07 -0600

    finish console edition

--- main.js
+++ main.js
@@ -1,14 +1,11 @@
 /* Notes to future self : 12-22-21
-    * You need to make sure the input from the user get's looped 
-    back if it's not an acceptable input. I've done it before and it's
-    on the tip of my tongue... time to go to sleep.
     * Change it to where the game only stops once a person reaches 5 
     points instead of 5 rounds.
 */
 
 
 // variables
-let playerSelection; // = window.prompt("Rock, paper, or scissors?", "");
+let playerSelection; // window.prompt("Rock, paper, or scissors?", "");
 const computerSelection = computerPlay();
 let playerScore = 0;
 let computerScore = 0;
@@ -30,16 +27,10 @@ function computerPlay() {
 // game play
 
 function playRound(playerSelection, computerSelection) {
-   /* trying to figure out how to go from prompting user for correct term to starting the game
-    let tryAgain;
-    if(playerSelection.toLowerCase() != "rock" || 
-    playerSelection.toLowerCase() != "scissors" || 
-    playerSelection.toLowerCase() != "paper") {
-        tryAgain = window.prompt("Please enter rock, paper, or scissors:", "");
-    } */
+   
     console.log(playerSelection + " " + "vs" + " " + computerSelection);
-    let player = playerSelection.toLowerCase();
-    let computer = computerSelection.toLowerCase();
+    player = playerSelection.toLowerCase();
+    computer = computerSelection.toLowerCase();
     if (player == computer) {
         return "It's a tie! No points awarded."
     }
@@ -66,30 +57,39 @@ function playRound(playerSelection, computerSelection) {
             } else if (player == "scissors" && computer == "paper") {
                 playerScore = ++playerScore;
               return "Scissors cut paper, you get a point!"
-        }
+        }   else if (playerSelection.toLowerCase() != "rock" ||
+                    playerSelection.toLowerCase() != "scissors" ||
+                    playerSelection.toLowerCase() != "paper") {
+                    window.prompt("Invalid input, try again.");
+                    return "Invalid input, try again."
+    }
     }       
 
 // console.log(playRound(playerSelection, computerSelection));
 
 function game(playerSelection) {
     let i = 0;
-    console.log("Whoever has the most points after 5 rounds wins!")
-    for(i = 0; i < 5; i++){
+    let rounds;
+    let winner = 50;
+    console.log("Whoever gets to 5 points wins the game!")
+    for(i = 0; i < winner; i++) {
         //stuff in the for loop..
         playerSelection = window.prompt("Rock, paper, or scissors?", "");
-        let rounds = playRound(playerSelection, computerPlay());
+        rounds = playRound(playerSelection, computerPlay());
         console.log(rounds);
         console.log(playerScore);
         console.log(computerScore);
+     
+        if(playerScore == 5 && computerScore != 5) {
+            winner = playerScore;
+            console.log("\n" + "You win! Congratulations. Refresh the page to play again!");
+        } else if (computerScore == 5 && playerScore != 5) {
+            winner = computerScore;
+            console.log("\n" + "You lost. You suck. How does it feel? Refresh the page to try again.")
+        } else if (playerScore == 5 && computerScore == 5) {
+            console.log("Looks like a tie. Refresh the page to try again!");
+        }
      }
-
-    if(playerScore > computerScore) {
-    console.log("\n" + "You win! Congratulations. Refresh the page to play again!");
-    } else if (playerScore < computerScore) {
-        console.log("\n" + "You lost. You suck. How does it feel? Refresh the page to try again.")
-    } else if (playerScore == computerScore) {
-        console.log("Looks like a tie. Refresh the page to try again!");
-    }
 }
 
 game(playerSelection);
