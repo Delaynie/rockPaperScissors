95a81457dc1f388fdb68b8ff409a0f75f15a6080
Delaynie Wheeler <delaynie93@gmail.com>
2022-01-08 14:32:47 -0600

    functionality with dom is finished

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const choices = document.querySelectorAll('.choice');
-const score = document.getElementById('.score');
-const result = document.getElementById('.result');
-const restart = document.getElementById('.restart');
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
 const modal = document.querySelector('.modal');
 const scoreboard = {
     player: 0,
@@ -9,11 +9,99 @@ const scoreboard = {
 }
 //play
 function play(e) {
-    console.log(e.target.id)
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(playerChoice, computerChoice, winner)
+    showWinner(winner, computerChoice);
+}
+
+// get computer choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if(rand < 0.34) {
+        return 'rock';
+    } else if(rand <= 0.67) {
+        return 'paper'
+    } else {
+        return 'scissors'
+    }
+}
+
+//get winner
+function getWinner(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return 'draw'
+    } else if (playerChoice === 'rock') {
+        if(computerChoice === 'paper') {
+            return 'computer';
+        } else {
+            return 'player'
+        }
+    } else if (playerChoice === 'paper') {
+        if(computerChoice === 'scissors') {
+            return 'computer';
+        } else {
+            return 'player'
+        }
+    } else if (playerChoice === 'scissors'){
+        if(computerChoice === 'rock') {
+            return 'computer';
+        } else {
+            return 'player'
+        }
+    }
+}
+
+//show winner
+function showWinner(winner, computerChoice) {
+    if(winner === 'player') {
+        scoreboard.player = scoreboard.player + 1;
+        result.innerHTML = 
+        `<h1 class="win">You Win!</h1>
+        <i class="img-${computerChoice}"></i>
+        <p>Computer chose <strong>${computerChoice}</strong></p>`;
+    } else if (winner === 'computer') {
+        scoreboard.computer++;
+        result.innerHTML = 
+        `<h1 class="lose">You Lose!</h1>
+        <i class="img-${computerChoice}"></i>
+        <p>Computer chose <strong>${computerChoice}</strong></p>`;
+    } else {
+        result.innerHTML = 
+        `<h1>It's a draw!</h1>
+        <i class="img-${computerChoice}"></i>
+        <p>Computer chose <strong>${computerChoice}</strong></p>`;
+    }
+    score.innerHTML = `
+        <p>Player: ${scoreboard.player}</P>
+        <p>Computer: ${scoreboard.computer}</p>`
+        
+        modal.style.display = 'block';
+    }
+
+    // restart game
+    function restartGame() {
+        scoreboard.player = 0;
+        scoreboard.computer = 0;
+        score.innerHTML = `
+        <p>Player: 0</p>
+        <p>Computer: 0</P>`
+        restart.style.display = 'none';
+    }
+
+    //clear modal
+    function clearModal(e) {
+        if(e.target == modal) {
+            modal.style.display = 'none';
+    }
 }
 
 // event listeners
 choices.forEach(choice => choice.addEventListener('click', play));
+window.addEventListener('click', clearModal);
+restart.addEventListener('click', restartGame);
 
 /* ---------- ORIGINAL CONSOLE EDITION ----------- */
 /*
